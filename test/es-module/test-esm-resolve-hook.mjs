22cbc7bd5430bbe4087462339d506310611dd7c5
guybedford <guybedford@gmail.com>
2017-09-17 12:11:19 +0200

    linting fixes

--- test/es-module/test-esm-resolve-hook.mjs
+++ test/es-module/test-esm-resolve-hook.mjs
@@ -1,5 +1,6 @@
 // Flags: --experimental-modules --loader ./test/fixtures/es-module-loaders/js-loader.js
-import {namedExport} from '../fixtures/es-module-loaders/js-as-esm.js';
+/* eslint-disable required-modules */
+import { namedExport } from '../fixtures/es-module-loaders/js-as-esm.js';
 import assert from 'assert';
 import ok from './test-esm-ok.mjs';
 

a382d5b99e815cbc93c91450d6d2ea16b0ea2d1f
guybedford <guybedford@gmail.com>
2017-09-21 14:06:47 +0200

    add documentation

---
+++ test/es-module/test-esm-example-loader.js
@@ -0,0 +1,3 @@
+// Flags: --experimental-modules --loader ./test/fixtures/es-module-loaders/example-loader.mjs
+/* eslint-disable required-modules */
+import ok from './test-esm-ok.mjs';
---
+++ test/fixtures/es-module-loaders/example-loader.mjs
@@ -0,0 +1,30 @@
+import url from 'url';
+import path from 'path';
+import process from 'process';
+
+const builtins = new Set(
+  Object.keys(process.binding('natives')).filter(str =>
+    /^(?!(?:internal|node|v8)\/)/.test(str))
+)
+const JS_EXTENSIONS = new Set(['.djs', '.dmjs']);
+export function resolve (specifier, parentModuleURL) {
+  if (builtins.has(specifier)) {
+    return {
+      url: specifier,
+      format: 'builtin'
+    };
+  }
+  if (/^\.{0,2}[\/]/.test(specifier) !== true) {
+    // (node_modules resolution goes here)
+    throw new Error(`imports must begin with '/', './', or '../'; '${specifier}' does not`);
+  }
+  const resolved = new URL(specifier, base);
+  const ext = path.extname(resolved.pathname);
+  if (!JS_EXTENSIONS.has(ext)) {
+    throw Error(`Cannot load file with non-JavaScript file extension ${ext}.`);
+  }
+  return {
+    url: resolved.href,
+    format: 'esm'
+  };
+}

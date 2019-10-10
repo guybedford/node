38554bc1ca35a9dd99789e44bd61b38ae582c92a
Guy Bedford <guybedford@gmail.com>
2019-10-10 18:12:21 -0400

    fixup test

--- test/es-module/test-cjs-esm-warn.js
+++ test/es-module/test-cjs-esm-warn.js
@@ -4,6 +4,7 @@ const common = require('../common');
 const fixtures = require('../common/fixtures');
 const { spawn } = require('child_process');
 const assert = require('assert');
+const path = require('path');
 
 const entry = fixtures.path('/es-modules/cjs-esm.js');
 
@@ -17,8 +18,8 @@ child.on('close', common.mustCall((code, signal) => {
   assert.strictEqual(code, 0);
   assert.strictEqual(signal, null);
   assert.strictEqual(stderr, `(node:${child.pid}) Warning: require() of .js ` +
-      `file ${entry} is not supported as it is an ES module due to having ` +
-      '"type": "module" in its package.json file.\nRather use import to load ' +
-      'this module, or if you are the author you may want to use the .cjs ' +
-      'extension for this file.\n');
+      `file ${path.resolve(entry, '../package-type-module/cjs.js')} is not ` +
+      'supported as it is an ES module due to having "type": "module" in its ' +
+      'package.json file.\nRather use import to load this module, or if you ' +
+      'are the author you may want to use the .cjs extension for this file.\n');
 }));
